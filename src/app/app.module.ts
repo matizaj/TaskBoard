c26e6f8f89e3515fa7e73279c3c2ca44a4511d08
mzajac <matzaj101@gmail.com>
2019-01-06 23:10:06 +0100

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { TodoTaskComponent } from './todo-task/todo-task.component';
+import { DoneTaskComponent } from './done-task/done-task.component';
+import { TaskService } from './_services/task.service';
+import { HttpService } from './_services/http.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    TodoTaskComponent,
+    DoneTaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [TaskService, HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
